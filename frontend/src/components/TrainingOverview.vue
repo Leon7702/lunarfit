a96e4aa5b1cc86f59b49d0c483c2b4819a12df58
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-05-13 15:32:04 +0200

    frontend: replaced src/components folder

---
+++ frontend/src/components/TrainingOverview.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <h2> >> Hier soll die Trainings Komponente sein.<< </h2>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/ZyklusOverview.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <h2> >> Hier soll die Zyklus Komponente sein.<< </h2>
+</template>
+
+<style scoped></style>
