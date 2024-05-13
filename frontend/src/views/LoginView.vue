6ce984644ae58c615f9042fde6d4faf2f5d2c1b5
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-05-13 15:33:43 +0200

    frontend: replaced src/views folder

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,198 @@
+<template>
+    <header>
+        <img alt="Lunafit logo" class="logo" src="@/assets/LunaFit.png" height="45" />
+    </header>
+    <div>
+        <form @submit="submitForm">
+            <div class="input-icon">
+                <img class="left-icon" src="@/assets/Communication.svg" alt="Email Icon" />
+                <input type="email" id="email" v-model="email" placeholder="E-Mail" required>
+            </div>
+
+            <div class="input-icon">
+                <img class="left-icon" src="@/assets/System.svg" alt="Password Icon" />
+                <input type="password" id="password" v-model="password" placeholder="Passwort" required>
+                <img class="right-icon" src="@/assets//Non-categorized.svg" alt="Eye closed Icon" />
+            </div>
+
+            <p class="forgot-password">
+                <a href="https://git.myhki.de/imi-bachelor-24" target="_blank">Passwort vergessen?</a>
+            </p>
+            <button type="submit">Anmelden</button>
+        </form>
+    </div>
+    <p style="text-align: center;">
+        Noch keinen Account?
+        <a href="/register">Jetzt registrieren</a>
+    </p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        register() {
+            // Hier kannst du den Registrierungsprozess implementieren
+            // Zum Beispiel: eine API-Anfrage an den Server senden
+            console.log('Registrierung abgeschickt');
+        }
+    }
+};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&display=swap');
+
+header {
+    line-height: 1.5;
+}
+
+body {
+    margin: 0;
+    line-height: normal;
+}
+
+h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+}
+
+h3 {
+    font-size: 1.2rem;
+}
+
+input {
+    display: block;
+    margin-bottom: 10px;
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+}
+
+button {
+    position: relative;
+    border-radius: 36px;
+    background-color: #50c1ba;
+    width: 80%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 60px;
+    margin: 50px auto;
+    /* Add 'auto' to center the button horizontally */
+    box-sizing: border-box;
+    cursor: pointer;
+    text-align: center;
+    font-size: 20px;
+    color: #fff;
+    font-family: Inter;
+    border: none;
+    /* Add this line to remove the gray border */
+}
+
+p {
+    font-size: 14px;
+}
+
+a {
+    color: #50c1ba;
+    font-weight: bold;
+    font-size: 14px;
+}
+
+.logo {
+    display: block;
+    margin: 4rem auto 8rem;
+}
+
+.forgot-password {
+    text-align: right;
+    font-size: 14px;
+}
+
+.input-icon {
+    position: relative;
+}
+
+.forgot-password {
+    margin-top: 18px;
+    margin-bottom: 65px;
+}
+
+/*
+.input-icon>img {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    width: 26px;
+    height: 26px;
+} 
+*/
+
+.input-icon>.left-icon {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 26px;
+    /* Adjust as needed */
+    height: 26px;
+    /* Adjust as needed */
+}
+
+.input-icon>.right-icon {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 26px;
+    /* Adjust as needed */
+    height: 26px;
+    /* Adjust as needed */
+}
+
+.input-icon>input {
+    width: 100%;
+    border: 1px solid #ececec;
+    box-sizing: border-box;
+    padding-left: 30px;
+    height: 47px;
+}
+
+::placeholder {
+    color: #959595;
+    padding-left: 20px;
+    opacity: 1;
+    position: relative;
+    font-size: 16px;
+    font-family: Inter;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+}
+</style>
