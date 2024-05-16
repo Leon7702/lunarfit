2c9d2c193df09db950fc7c8ae9189de63f9650c0
Jonas Trenkler <jonas-trenkler@gmx.de>
2024-05-16 22:45:21 +0200

    feat(auth): add jwt token pair auth using Simple JWT
    Implement the basic JWT token authorization following https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html It uses a token pair: short lived `access` and a `refresh` token to request a new `access` token. The endpoints are /api/token/ and /api/token/refresh/ for now. The /user/ was temporarily changed to allow all access, so a user can be created for testing the frontend.
    
    Ref projectdocumentation#30

--- backend/lunarfit/urls.py
+++ backend/lunarfit/urls.py
@@ -14,18 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from lunarfit.quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
