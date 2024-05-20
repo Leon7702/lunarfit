e1d20f6e4fa33962b8a649a8bcfce4210cfeb6fa
leon :) <leon.snakowski@web.de>
2024-05-20 12:43:38 +0200

    frontend: added LoginPage + Link between pages

--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -38,12 +38,11 @@
     </q-input>
   </div>
   <div class="q-pa-md q-gutter-sm row justify-center">
-    <q-btn no-caps rounded style="background: #50C1BA; color: white" label="Registrieren" padding="xs lg" size="16px"/>
+    <q-btn no-caps rounded style="background: #50C1BA; color: white" label="Registrieren" padding="sm lg" size="16px"/>
   </div>
   <p style="text-align: center;">
     Du hast einen Account?
-    <a href="/login">Jetzt anmelden</a>
-  </p>
+    <router-link to="/login">Jetzt registrieren</router-link>  </p>
 </template>
 
 <script>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -10,6 +10,10 @@ const routes = [
     path: '/register',
     component: () => import('pages/RegisterPage.vue')
   },
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage.vue')
+  },
 
   // Always leave this as last one,
   // but you can also remove it
