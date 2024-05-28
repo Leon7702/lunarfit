4ab2634d9ae33a76ed43dcf068e79c16f4f9b593
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-05-29 00:36:29 +0200

    frontend: adapt TrsCycle like Figma Screen

--- frontend/src/components/TrsSunburst.vue
+++ frontend/src/components/TrsSunburst.vue
@@ -5,82 +5,82 @@
         <!-- <circle class="donut-hole" cx="21" cy="21" r="10" fill="transparent"></circle> -->
         <!-- <circle class="donut-ring" cx="10" cy="10" r="6" fill="000" stroke="#50C1BA" stroke-width="3"></circle> -->
         <!-- 54/ pi = 17,18873385-->
-        <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="27 81" stroke-dashoffset="0"></circle>
-
         <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="27 81" stroke-dashoffset="27"></circle>
+          stroke-width="3.5" stroke-dasharray="27 81" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="27 81" stroke-dashoffset="54"></circle>
+        <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#50C1BA"
+          stroke-width="3.5" stroke-dasharray="27 81" stroke-dashoffset="27"></circle>
 
         <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="27 81" stroke-dashoffset="81"></circle>
+          stroke-width="3.5" stroke-dasharray="27 81" stroke-dashoffset="54"></circle>
 
-        <!-- 49/pi = 15,59718442 -->
-        <circle class="donut-segment-5" cx="21" cy="21" r="15.59718442" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="24.5 73.5" stroke-dashoffset="0"></circle>
+        <circle class="donut-segment-6" cx="21" cy="21" r="17.18873385" fill="transparent" stroke="#2D8781"
+          stroke-width="3.5" stroke-dasharray="27 81" stroke-dashoffset="81"></circle>
 
-        <circle class="donut-segment-5" cx="21" cy="21" r="15.59718442" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="24.5 73.5" stroke-dashoffset="24.5"></circle>
+        <!-- 43/pi = 13,68732511 -->
+        <circle class="donut-segment-5" cx="21" cy="21" r="13.68732511" fill="transparent" stroke="#9CD3D0"
+          stroke-width="3" stroke-dasharray="21.5 64.5" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-5" cx="21" cy="21" r="15.59718442" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="24.5 73.5" stroke-dashoffset="49"></circle>
+        <circle class="donut-segment-5" cx="21" cy="21" r="13.68732511" fill="transparent" stroke="#50C1BA"
+          stroke-width="3" stroke-dasharray="21.5 64.5" stroke-dashoffset="21.5"></circle>
 
-        <circle class="donut-segment-5" cx="21" cy="21" r="15.59718442" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="24.5 73.5" stroke-dashoffset="73.5"></circle>
+        <circle class="donut-segment-5" cx="21" cy="21" r="13.68732511" fill="transparent" stroke="#93EDE8"
+          stroke-width="3" stroke-dasharray="21.5 64.5" stroke-dashoffset="43"></circle>
 
-        <!-- 44/pi = 14,00563499 -->
-        <circle class="donut-segment-4" cx="21" cy="21" r="14.00563499" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="22 66" stroke-dashoffset="0"></circle>
+        <circle class="donut-segment-5" cx="21" cy="21" r="13.68732511" fill="transparent" stroke="#2D8781"
+          stroke-width="3" stroke-dasharray="21.5 64.5" stroke-dashoffset="64.5"></circle>
 
-        <circle class="donut-segment-4" cx="21" cy="21" r="14.00563499" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="22 66" stroke-dashoffset="22"></circle>
+        <!-- 33,5/pi = 10,66338119 -->
+        <circle class="donut-segment-4" cx="21" cy="21" r="10.66338119" fill="transparent" stroke="#9CD3D0"
+          stroke-width="2.5" stroke-dasharray="16.75 50.25" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-4" cx="21" cy="21" r="14.00563499" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="22 66" stroke-dashoffset="44"></circle>
+        <circle class="donut-segment-4" cx="21" cy="21" r="10.66338119" fill="transparent" stroke="#50C1BA"
+          stroke-width="2.5" stroke-dasharray="16.75 50.25" stroke-dashoffset="16.75"></circle>
 
-        <circle class="donut-segment-4" cx="21" cy="21" r="14.00563499" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="22 66" stroke-dashoffset="66"></circle>
+        <circle class="donut-segment-4" cx="21" cy="21" r="10.66338119" fill="transparent" stroke="#93EDE8"
+          stroke-width="2.5" stroke-dasharray="16.75 50.25" stroke-dashoffset="33.5"></circle>
 
-        <!-- 39/pi = 12,41408556 -->
-        <circle class="donut-segment-3" cx="21" cy="21" r="12.41408556" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="19.5 58.5" stroke-dashoffset="0"></circle>
+        <circle class="donut-segment-4" cx="21" cy="21" r="10.66338119" fill="transparent" stroke="#2D8781"
+          stroke-width="2.5" stroke-dasharray="16.75 50.25" stroke-dashoffset="50.25"></circle>
 
-        <circle class="donut-segment-3" cx="21" cy="21" r="12.41408556" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="19.5 58.5" stroke-dashoffset="19.5"></circle>
+        <!-- 25,5/pi = 8,116902098 -->
+        <circle class="donut-segment-3" cx="21" cy="21" r="8.116902098" fill="transparent" stroke="#9CD3D0"
+          stroke-width="2" stroke-dasharray="12.75 38.25" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-3" cx="21" cy="21" r="12.41408556" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="19.5 58.5" stroke-dashoffset="39"></circle>
+        <circle class="donut-segment-3" cx="21" cy="21" r="8.116902098" fill="transparent" stroke="#50C1BA"
+          stroke-width="2" stroke-dasharray="12.75 38.25" stroke-dashoffset="12.75"></circle>
 
-        <circle class="donut-segment-3" cx="21" cy="21" r="12.41408556" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="19.5 58.5" stroke-dashoffset="58.5"></circle>
+        <circle class="donut-segment-3" cx="21" cy="21" r="8.116902098" fill="transparent" stroke="#93EDE8"
+          stroke-width="2" stroke-dasharray="12.75 38.25" stroke-dashoffset="25.5"></circle>
 
-        <!-- 34/pi = 10,82253613 -->
-        <circle class="donut-segment-2" cx="21" cy="21" r="10.82253613" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="17 51" stroke-dashoffset="0"></circle>
+        <circle class="donut-segment-3" cx="21" cy="21" r="8.116902098" fill="transparent" stroke="#2D8781"
+          stroke-width="2" stroke-dasharray="12.75 38.25" stroke-dashoffset="38.25"></circle>
 
-        <circle class="donut-segment-2" cx="21" cy="21" r="10.82253613" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="17 51" stroke-dashoffset="17"></circle>
+        <!-- 19/pi = 6,047887837 -->
+        <circle class="donut-segment-2" cx="21" cy="21" r="6.047887837" fill="transparent" stroke="#9CD3D0"
+          stroke-width="1.5" stroke-dasharray="9.5 28.5" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-2" cx="21" cy="21" r="10.82253613" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="17 51" stroke-dashoffset="34"></circle>
+        <circle class="donut-segment-2" cx="21" cy="21" r="6.047887837" fill="transparent" stroke="#50C1BA"
+          stroke-width="1.5" stroke-dasharray="9.5 28.5" stroke-dashoffset="9.5"></circle>
 
-        <circle class="donut-segment-2" cx="21" cy="21" r="10.82253613" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="17 51" stroke-dashoffset="51"></circle>
+        <circle class="donut-segment-2" cx="21" cy="21" r="6.047887837" fill="transparent" stroke="#93EDE8"
+          stroke-width="1.5" stroke-dasharray="9.5 28.5" stroke-dashoffset="19"></circle>
 
-        <!-- 29/pi = 9,230986699 -->
-        <circle class="donut-segment-1" cx="21" cy="21" r="9.230986699" fill="transparent" stroke="#50C1BA"
-          stroke-width="1.5" stroke-dasharray="14.5 43.5" stroke-dashoffset="0"></circle>
+        <circle class="donut-segment-2" cx="21" cy="21" r="6.047887837" fill="transparent" stroke="#2D8781"
+          stroke-width="1.5" stroke-dasharray="9.5 28.5" stroke-dashoffset="28.5"></circle>
 
-        <circle class="donut-segment-1" cx="21" cy="21" r="9.230986699" fill="transparent" stroke="#9CD3D0"
-          stroke-width="1.5" stroke-dasharray="14.5 43.5" stroke-dashoffset="14.5"></circle>
+        <!-- 14/pi = 4,456338407 -->
+        <circle class="donut-segment-1" cx="21" cy="21" r="4.456338407" fill="transparent" stroke="#9CD3D0"
+          stroke-width="1" stroke-dasharray="7 21" stroke-dashoffset="0"></circle>
 
-        <circle class="donut-segment-1" cx="21" cy="21" r="9.230986699" fill="transparent" stroke="#2D8781"
-          stroke-width="1.5" stroke-dasharray="14.5 43.5" stroke-dashoffset="29"></circle>
+        <circle class="donut-segment-1" cx="21" cy="21" r="4.456338407" fill="transparent" stroke="#50C1BA"
+          stroke-width="1" stroke-dasharray="7 21" stroke-dashoffset="7"></circle>
 
-        <circle class="donut-segment-1" cx="21" cy="21" r="9.230986699" fill="transparent" stroke="#93EDE8"
-          stroke-width="1.5" stroke-dasharray="14.5 43.5" stroke-dashoffset="43.5"></circle>
+        <circle class="donut-segment-1" cx="21" cy="21" r="4.456338407" fill="transparent" stroke="#93EDE8"
+          stroke-width="1" stroke-dasharray="7 21" stroke-dashoffset="14"></circle>
+
+        <circle class="donut-segment-1" cx="21" cy="21" r="4.456338407" fill="transparent" stroke="#2D8781"
+          stroke-width="1" stroke-dasharray="7 21" stroke-dashoffset="21"></circle>
       </svg>
     </div>
 
