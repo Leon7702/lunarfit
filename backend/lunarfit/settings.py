11ac0e7f7bafcb24058ac2819b9e55d9b32980c2
Jonas Trenkler <jonas-trenkler@gmx.de>
2024-06-01 15:21:47 +0200

    feat(backend): re-enable http SessionAuthentication
    Re-enable SessionAuthentication for the backend, as it is used by the
    DRF api-auth and can be used for the other browsable API frontends.
    
    Ref: projectdocumentation#36

--- backend/lunarfit/settings.py
+++ backend/lunarfit/settings.py
@@ -62,6 +62,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
