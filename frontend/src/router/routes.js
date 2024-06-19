903e31784453f6b5add0286ed1be83895e5ca34e
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-06-19 22:00:46 +0200

    frontend: update routes

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     children: [
       {
         path: "",
-        component: () => import("pages/IndexPage.vue"),
+        component: () => import("pages/DashboardPage.vue"),
         meta: { requiresAuth: true },
       },
       {
