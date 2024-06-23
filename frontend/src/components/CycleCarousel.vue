160bf3bbc13d76fc1dd580a888f2b3b6cd6d1846
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-06-24 00:31:31 +0200

    frontend: add menstruationInfo to carousel slide

--- frontend/src/components/CycleCarousel.vue
+++ frontend/src/components/CycleCarousel.vue
@@ -19,15 +19,86 @@
         <q-carousel-slide name="menstruation" class="column no-wrap flex-center">
           <q-scroll-area class="fit">
             <div class="column no-wrap flex-center">
-              <div class="text-h6">Menstruation</div>
+              <div class="text-h6">{{ $t('menstruationInfo.title') }}</div>
               <q-card flat bordered class="my-card">
                 <q-card-section>
-                  {{ data.menstruationtext }}
+                  {{ $t('menstruationInfo.description') }}
                 </q-card-section>
               </q-card>
               <img class="cycle-image" src="../assets/cyclePhase/PeriodOvulation.png" alt="Graph" />
-              <PhaseInformation :textNutrition="data.menstruationNutrition" :textTraining="data.menstruationTraining"
-                :textHealth="data.menstruationHealth" />
+              <!-- <PhaseInformation :textNutrition="data.menstruationNutrition" :textTraining="data.menstruationTraining"
+                :textHealth="data.menstruationHealth" /> -->
+
+              <div class="q-pt-md q-pb-md" style="width: 100%">
+                <q-list bordered class="rounded-borders">
+                  <q-expansion-item class="hover-effect">
+                    <template v-slot:header>
+                      <q-item-section avatar>
+                        <q-avatar>
+                          <img src="../assets/Food.svg" alt="Food" />
+                        </q-avatar>
+                      </q-item-section>
+                      <q-item-section>
+                        Ernährung
+                      </q-item-section>
+                    </template>
+                    <q-card>
+                      <q-card-section>
+                        <div>{{ $t('menstruationInfo.nutrition[0]') }}</div>
+                        <div>{{ $t('menstruationInfo.nutrition[1]') }}</div>
+                        <div>{{ $t('menstruationInfo.nutrition[2]') }}</div>
+                        <div>{{ $t('menstruationInfo.nutrition[3]') }}</div>
+                      </q-card-section>
+                    </q-card>
+                  </q-expansion-item>
+
+                  <q-separator />
+
+                  <q-expansion-item class="hover-effect">
+                    <template v-slot:header>
+                      <q-item-section avatar>
+                        <q-avatar>
+                          <img src="../assets/Training.svg" alt="Training" />
+                        </q-avatar>
+                      </q-item-section>
+                      <q-item-section>
+                        Training
+                      </q-item-section>
+                    </template>
+                    <q-card>
+                      <q-card-section>
+                        {{ $t('menstruationInfo.training') }}
+                      </q-card-section>
+                    </q-card>
+                  </q-expansion-item>
+
+                  <q-separator />
+
+                  <q-expansion-item class="hover-effect">
+                    <template v-slot:header>
+                      <q-item-section avatar>
+                        <q-avatar>
+                          <img src="../assets/Health.svg" alt="Food" />
+                        </q-avatar>
+                      </q-item-section>
+                      <q-item-section>
+                        Gesundheit/ Fruchtbarkeit
+                      </q-item-section>
+                    </template>
+                    <q-card>
+                      <q-card-section>
+                        <div>{{ $t('menstruationInfo.health[0]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[1]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[2]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[3]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[4]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[5]') }}</div>
+                        <div>{{ $t('menstruationInfo.health[6]') }}</div>
+                      </q-card-section>
+                    </q-card>
+                  </q-expansion-item>
+                </q-list>
+              </div>
             </div>
           </q-scroll-area>
         </q-carousel-slide>
@@ -160,5 +231,9 @@ export default {
 .cycle-image {
   max-width: 100%;
   padding-top: 16px;
+}
+
+li {
+  margin: 0;
 }
 </style>
