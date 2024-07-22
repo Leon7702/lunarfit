4c3195546add32d6f630307176bdbb4235f7f625
Jonas Trenkler <jonas-trenkler@gmx.de>
2024-07-22 17:10:05 +0200

    feat(backend): autoload initial data fixtures
    Instead of providing the initial data for keyword tables through a
    migration, we opted to use fixtures. These need to be loaded manually.
    A line is added to the startup script that loads all fixtures in every
    app in the default directory `fixtures`.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,5 +4,8 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 
+# load all fixtures in default fixtures directory of apps
+python3 manage.py loaddata ./*/fixtures/*.yaml
+
 # Run the dev server on 0.0.0.0 to make it accessible outside the container
 python3 manage.py runserver 0.0.0.0:8000
