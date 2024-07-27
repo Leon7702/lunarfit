74f5642bdb77bf382625414c53ed4010eaec8523
Lilly Mydlach <lillymydlach@gmail.com>
2024-07-28 00:30:49 +0200

    Add phase endpoint.

--- backend/cycles/urls.py
+++ backend/cycles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import MenstrualCycleViewSet, MedicationViewSet, MedicationCategoryViewSet, TrackingDataViewSet, TypeViewSet
+from .views import MenstrualCycleViewSet, MedicationViewSet, MedicationCategoryViewSet, TrackingDataViewSet, TypeViewSet, PhaseViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r"phases", MenstrualCycleViewSet, "cycles")
+router.register(r"cycles", MenstrualCycleViewSet, "cycles")
+router.register(r"phases", PhaseViewSet, "phases")
 router.register(r"log/type", TypeViewSet, "log-type")
 router.register(r"log", TrackingDataViewSet, "log")
 router.register(r"medication/category", MedicationCategoryViewSet, "medication-category")
