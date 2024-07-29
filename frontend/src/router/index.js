98642a929e3e44313e2e948986e0e653cc99f8de
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-07-29 16:24:00 +0200

    frontend: change createWebHistory to createWebHashHistory...

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 import routes from './routes';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
