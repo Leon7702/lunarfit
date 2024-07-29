465ee8ff39a8a85265826a694edce7f3af49499a
Kieu Truc Tran <kieutruc.tran@student.htw-berlin.de>
2024-07-29 22:46:30 +0200

    Revert "frontend: change createWebHistory to createWebHashHistory..."
    This reverts commit 98642a929e3e44313e2e948986e0e653cc99f8de.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 import routes from './routes';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
