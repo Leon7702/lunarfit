e778defb195d4915693b9683a752716925a69d6a
Jonas Trenkler <jonas-trenkler@gmx.de>
2024-08-05 23:39:02 +0200

    feat(admin): add display, filters of training data
    Adds model admins and filters to improve the list view of TRS entries
    and workouts in the backends admin interface.

--- backend/training/admin.py
+++ backend/training/admin.py
@@ -2,11 +2,31 @@ from django.contrib import admin
 
 from .models import Sport, Trs, Workout
 
+
 class SportAdmin(admin.ModelAdmin):
     model = Sport
     list_display = ["name", "id"]
     readonly_fields = ["id"]
 
+
+class TrsAdmin(admin.ModelAdmin):
+    model = Trs
+    list_display = [field.name for field in model._meta.fields]
+    list_filter = [
+        ("user", admin.RelatedOnlyFieldListFilter),
+    ]
+    readonly_fields = ["acwr", "trs_acwr"]
+
+
+class WorkoutAdmin(admin.ModelAdmin):
+    model = Workout
+    list_display = [field.name for field in model._meta.fields]
+    list_filter = [
+        ("user", admin.RelatedOnlyFieldListFilter),
+        ("sport", admin.RelatedOnlyFieldListFilter),
+    ]
+
+
 admin.site.register(Sport, SportAdmin)
-admin.site.register(Trs)
-admin.site.register(Workout)
+admin.site.register(Trs, TrsAdmin)
+admin.site.register(Workout, WorkoutAdmin)
